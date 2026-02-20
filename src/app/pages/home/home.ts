519c693f1bff2d408f08274073a778b1b6e3d90a
CarolMontteiro <carolmonteiro1206@gmail.com>
2026-02-20 20:26:46 -0300

    Imagem do slider sendo renderizada e assets configurado corretamente

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Slider } from './slider/slider';
 import { ProductCategory } from './product-category/product-category';
 import { FeaturedProducts } from './featured-products/featured-products';
+import { sliderItem } from '../../models/sliderItem.model';
 
 @Component({
   selector: 'app-home',
@@ -10,5 +11,21 @@ import { FeaturedProducts } from './featured-products/featured-products';
   styleUrl: './home.css',
 })
 export class Home {
-
+  public sliderData: sliderItem[] = [
+    {
+      id:'1',
+      image: 'assets/images/slider/slider1.webp',
+      alt:'Promoção'
+    },
+    {
+      id:'2',
+      image: 'assets/images/slider/slider2.webp',
+      alt:'Segurança e Sastifação'
+    },
+    {
+      id:'3',
+      image: 'assets/images/slider/slider3.webp',
+      alt:'Casa e cozinha'
+    },
+  ];
 }
